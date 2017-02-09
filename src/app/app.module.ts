f5819d5f98d98672a9ebf526a384154008e83fde
Zara Siddique <zara@siddique.eu>
2017-02-09 11:49:44 +0000

    Angular Material working.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
+import 'hammerjs';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
+import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { LoginViewComponent } from './login-view/login-view.component';
@@ -19,6 +22,7 @@ import { LandingViewComponent } from './landing-view/landing-view.component';
     FormsModule,
     HttpModule,
     AppRoutingModule,
+    MaterialModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
