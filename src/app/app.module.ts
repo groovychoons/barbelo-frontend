d1d778eba67932f1092465dd5fca8405a9219f3d
Zara Siddique <zara@siddique.eu>
2017-02-09 14:19:12 +0000

    Basics of login form working.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import 'hammerjs';
 
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
 import { MaterialModule } from '@angular/material';
+
+import { AUTH_PROVIDERS } from 'angular2-jwt';
+import { AuthGuard } from './common/auth.guard';
 
 import { AppComponent } from './app.component';
 import { LoginViewComponent } from './login-view/login-view.component';
@@ -24,7 +28,7 @@ import { LandingViewComponent } from './landing-view/landing-view.component';
     AppRoutingModule,
     MaterialModule.forRoot(),
   ],
-  providers: [],
+  providers: [ AuthGuard, ...AUTH_PROVIDERS],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
