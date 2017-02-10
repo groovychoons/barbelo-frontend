9b6f2fd23131f5106db96f4d0cb34677c55c60ff
Zara Siddique <zara@siddique.eu>
2017-02-10 10:37:26 +0000

    Search bar added and minor design changes.

--- src/app/MOCK_PROJECT_DATA.ts
+++ src/app/MOCK_PROJECT_DATA.ts
@@ -38,5 +38,76 @@ export var MOCK_PROJECT_DATA: Card[] = [
 	currentFunding: 100,
     imgSrc: 'http://localhost:4200/assets/slide-03.jpeg'
 },
-];
-
+{
+	id: 5,
+	category: 'Volunteering',
+	title: 'Help out a brother',
+	description: 'A platform for change, we hope to redefine the world of crowdfunding.',
+	fundingGoal: 8800,
+	currentFunding: 8500,
+    imgSrc: 'http://localhost:4200/assets/slide-02.jpeg'
+},
+{
+	id: 6,
+	category: 'Technology',
+	title: 'Introducing Barbelo',
+	description: 'A platform for change, we hope to redefine the world of crowdfunding.',
+	fundingGoal: 10000,
+	currentFunding: 100,
+	imgSrc: 'http://localhost:4200/assets/slide-00.jpeg',
+},
+{
+	id: 7,
+	category: 'Community',
+	title: 'Cardiff Cribs',
+	description: 'Building a new town centre for all the beautiful residents of Cardiff, we hope to change the city for better.',
+	fundingGoal: 1300,
+	currentFunding: 932,
+	imgSrc: 'http://localhost:4200/assets/slide-01.jpeg',
+},
+{
+	id: 8,
+	category: 'Innovation',
+	title: 'Introducing Charlebones',
+	description: 'A platform for change, we hope to redefine the world of crowdfunding.',
+	fundingGoal: 10000,
+	currentFunding: 100,
+	imgSrc: 'http://localhost:4200/assets/slide-02.jpeg'
+},
+{
+	id: 9,
+	category: 'Volunteering',
+	title: 'Introducing Charlebones',
+	description: 'A platform for change, we hope to redefine the world of crowdfunding.',
+	fundingGoal: 650,
+	currentFunding: 100,
+    imgSrc: 'http://localhost:4200/assets/slide-03.jpeg'
+},
+{
+	id: 10,
+	category: 'Volunteering',
+	title: 'Help out a brother',
+	description: 'A platform for change, we hope to redefine the world of crowdfunding.',
+	fundingGoal: 8800,
+	currentFunding: 8500,
+    imgSrc: 'http://localhost:4200/assets/slide-02.jpeg'
+},
+{
+	id: 11,
+	category: 'Technology',
+	title: 'Introducing Barbelo',
+	description: 'A platform for change, we hope to redefine the world of crowdfunding.',
+	fundingGoal: 10000,
+	currentFunding: 100,
+	imgSrc: 'http://localhost:4200/assets/slide-00.jpeg',
+},
+{
+	id: 12,
+	category: 'Community',
+	title: 'Cardiff Cribs',
+	description: 'Building a new town centre for all the beautiful residents of Cardiff, we hope to change the city for better.',
+	fundingGoal: 1300,
+	currentFunding: 932,
+	imgSrc: 'http://localhost:4200/assets/slide-01.jpeg',
+},
+]
